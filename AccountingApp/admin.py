2674fad6f74959a0a04e0b2a860cdcd37480c95d
Ehsan Karbasian <ehsan.karbasian@gmail.com>
2021-09-23 17:46:47 +0330

    database tables implemented

--- AccountingApp/admin.py
+++ AccountingApp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Room)
+admin.site.register(Spend)
+admin.site.register(Spenders)
+admin.site.register(Partners)
+admin.site.register(Person)
+admin.site.register(Transaction)
--- AccountingApp/models.py
+++ AccountingApp/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+
+
+class Room(models.Model):
+    created_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=100, default="new_room")
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Spend(models.Model):
+    amount = models.IntegerField(default=0)
+    description = models.CharField(max_length=256, blank=True)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    spenders = models.ForeignKey("Spenders", on_delete=models.DO_NOTHING)
+    partners = models.ForeignKey("Partners", on_delete=models.DO_NOTHING)
+
+
+class Spenders(models.Model):
+    weight = models.IntegerField(default=1)
+
+    class Meta:
+        verbose_name_plural = 'spender (Person m2m Spend)'
+
+
+class Partners(models.Model):
+    weight = models.IntegerField(default=1)
+
+    class Meta:
+        verbose_name_plural = 'partner (Person m2m Spend)'
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=100, default="new_person")
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    spenders = models.ForeignKey("Spenders", on_delete=models.DO_NOTHING)
+    partners = models.ForeignKey("Partners", on_delete=models.DO_NOTHING)
+
+
+class Transaction(models.Model):
+    payer = models.ForeignKey("Person", related_name="payer", on_delete=models.DO_NOTHING)
+    receiver = models.ForeignKey("Person", related_name="receiver", on_delete=models.DO_NOTHING)
+    date = models.DateTimeField(auto_now=True)
+    amount = models.IntegerField(default=0)
