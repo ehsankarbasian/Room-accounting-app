7d354b701c8fb748bf1e28a08d35df0e6325d19e
ehasan-karbasian <ehsan.karbasian@gmail.com>
2025-08-23 21:08:44 +0330

    Debug: ReportGraph constructor

--- AccountingApp/algorithm/graph.py
+++ AccountingApp/algorithm/graph.py
@@ -7,6 +7,10 @@ from AccountingApp.algorithm.room_analyzer import RoomAnalyzer
 class ReportGraph(_DirectedGraph):
     
     def __init__(self, room, incoming_graph_data=None, **attr):
+        super().__init__(incoming_graph_data, **attr)
+        # If delete the line above, Error will happen:
+        # '_CachedPropertyResetterAdjAndSucc' object is not subscriptable
+        
         self._room = room
         self.potential_edges = RoomAnalyzer.get_room_potential_edges(room)
         RoomAnalyzer.calculate_room_spend_result(room, self)
@@ -19,27 +23,14 @@ class ReportGraph(_DirectedGraph):
     
     def add_edge(self, u_of_edge, v_of_edge, **kwargs):
         if kwargs.get('weight', None) is not None:
-            try:
-                if self.has_edge(u_of_edge, v_of_edge):
-                    previous_weight = self.get_edge_data(u_of_edge, v_of_edge)['weight']
-                    kwargs['weight'] += previous_weight
-                elif self.has_edge(v_of_edge, u_of_edge):
-                    previous_weight = self.get_edge_data(v_of_edge, u_of_edge)['weight']
-                    kwargs['weight'] -= previous_weight
-                    if self.has_edge(v_of_edge, u_of_edge):
-                        self.remove_edge(v_of_edge, u_of_edge)
-            except:
-                a = 2
-            
-            
-            # if self.has_edge(u_of_edge, v_of_edge):
-            #     previous_weight = self.get_edge_data(u_of_edge, v_of_edge)['weight']
-            #     kwargs['weight'] += previous_weight
-            # elif self.has_edge(v_of_edge, u_of_edge):
-            #     previous_weight = self.get_edge_data(v_of_edge, u_of_edge)['weight']
-            #     kwargs['weight'] -= previous_weight
-            #     if self.has_edge(v_of_edge, u_of_edge):
-            #         self.remove_edge(v_of_edge, u_of_edge)
+            if self.has_edge(u_of_edge, v_of_edge):
+                previous_weight = self.get_edge_data(u_of_edge, v_of_edge)['weight']
+                kwargs['weight'] += previous_weight
+            elif self.has_edge(v_of_edge, u_of_edge):
+                previous_weight = self.get_edge_data(v_of_edge, u_of_edge)['weight']
+                kwargs['weight'] -= previous_weight
+                if self.has_edge(v_of_edge, u_of_edge):
+                    self.remove_edge(v_of_edge, u_of_edge)
         
         if kwargs.get('weight') < 0:
             if self.has_edge(u_of_edge, v_of_edge):
