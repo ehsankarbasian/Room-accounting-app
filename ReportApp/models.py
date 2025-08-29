0c4a468e16396f0aec52ef524fa41df33cb4778a
ehasan-karbasian <ehsan.karbasian@gmail.com>
2025-08-29 20:02:40 +0330

    Debug: delete room

--- ReportApp/models.py
+++ ReportApp/models.py
@@ -47,7 +47,7 @@ class Token(models.Model):
 class Room(models.Model):
     created_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=100, default="new_room")
-    creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     class Meta:
         verbose_name_plural = verbose_name_plural('Rooms')
@@ -116,8 +116,8 @@ class Spend(models.Model):
 
 class Spenders(models.Model):
     weight = models.IntegerField(default=1)
-    spender_person = models.ForeignKey("Person", on_delete=models.DO_NOTHING, default=None)
-    spender_spend = models.ForeignKey("Spend", on_delete=models.DO_NOTHING, default=None)
+    spender_person = models.ForeignKey("Person", on_delete=models.CASCADE, default=None)
+    spender_spend = models.ForeignKey("Spend", on_delete=models.CASCADE, default=None)
 
     class Meta:
         verbose_name_plural = verbose_name_plural('spender (Person m2m Spend)')
@@ -128,8 +128,8 @@ class Spenders(models.Model):
 
 class Partners(models.Model):
     weight = models.IntegerField(default=1)
-    partner_person = models.ForeignKey("Person", on_delete=models.DO_NOTHING, default=None)
-    partner_spend = models.ForeignKey("Spend", on_delete=models.DO_NOTHING, default=None)
+    partner_person = models.ForeignKey("Person", on_delete=models.CASCADE, default=None)
+    partner_spend = models.ForeignKey("Spend", on_delete=models.CASCADE, default=None)
 
     class Meta:
         verbose_name_plural = verbose_name_plural('partner (Person m2m Spend)')
@@ -188,8 +188,8 @@ class Person(models.Model):
 
 
 class Transaction(models.Model):
-    payer = models.ForeignKey("Person", related_name="payer", on_delete=models.DO_NOTHING)
-    receiver = models.ForeignKey("Person", related_name="receiver", on_delete=models.DO_NOTHING)
+    payer = models.ForeignKey("Person", related_name="payer", on_delete=models.CASCADE)
+    receiver = models.ForeignKey("Person", related_name="receiver", on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now=True)
     amount = models.IntegerField(default=0)
 
