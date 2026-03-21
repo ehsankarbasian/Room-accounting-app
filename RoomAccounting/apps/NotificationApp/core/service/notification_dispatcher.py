8f259678720078fce9e1a95d6c3ad1126a3dd37c
Ehsan Karbasian <ehsan.karbasian@gmail.com>
2026-03-21 08:11:15 +0330

    Add data model type validation in dispatcher

--- RoomAccounting/apps/NotificationApp/core/service/notification_dispatcher.py
+++ RoomAccounting/apps/NotificationApp/core/service/notification_dispatcher.py
@@ -1,9 +1,10 @@
 from enum import Enum
+from dataclasses import is_dataclass
 
 from typing import Type
 
 from apps.NotificationApp.core.types import UserProtocol
-from apps.NotificationApp.core.registry import MapperRegistry
+from apps.NotificationApp.core.registry import MapperRegistry, DataModelRegistry
 
 from .message_factory import MessageFactory
 
@@ -17,10 +18,21 @@ class NotificationDispatcher:
             raise TypeError(
                 f"'message_type' must be an Enum instance, not {type(message_type).__name__}"
             )
-        
+
+        if not is_dataclass(data):
+            raise TypeError(
+                f"data must be a dataclass instance, got {type(data).__name__}"
+            )
+
+        expected_datamodel = DataModelRegistry.REGISTRY[message_type]
+        if not isinstance(data, expected_datamodel):
+            raise TypeError(
+                f"{message_type} expects {expected_datamodel.__name__} instance, "
+                f"got {type(data).__name__}"
+            )
+
+        mapper = MapperRegistry.REGISTRY[message_type]
+        canonical_data = mapper.map(data=data)
+
         sender = MessageFactory.get_sender(user=user)
-        
-        MapperClass = MapperRegistry.REGISTRY[message_type]
-        cononical_data = MapperClass.map(data=data)
-        
-        sender.send(message=cononical_data)
+        sender.send(message=canonical_data)
