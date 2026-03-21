b06da8a5120d82d678730b612505e1b03372bfd7
Ehsan Karbasian <ehsan.karbasian@gmail.com>
2026-03-21 05:19:12 +0330

    Refactor terminal sender

--- RoomAccounting/apps/NotificationContribApp/core/message_sender/terminal.py
+++ RoomAccounting/apps/NotificationContribApp/core/message_sender/terminal.py
@@ -1,38 +1,29 @@
-from apps.NotificationApp.core.registry import SenderRegistry, BuilderRegistry
-from apps.NotificationApp.core.interfaces import MessageSenderInterface, MessageBuilderInterface
+from apps.NotificationApp.core.registry import SenderRegistry
+from apps.NotificationApp.core.interfaces import MessageSenderInterface
+from apps.NotificationContribApp.core.models.canonical import NotificationCanonicalMessage
 
 from apps.NotificationContribApp.notification_types import SenderType
 
 
 @SenderRegistry.register(name=SenderType.TERMINAL)
 class TerminalSender(MessageSenderInterface):
-    
-    def __init__(self, message_type, data, identifier):
+
+    def __init__(self, identifier):
         self._to = identifier
-        self.build_payload(message_type, data)
-    
-    
-    def build_payload(self, message_type, data):
-        builder: MessageBuilderInterface = BuilderRegistry.REGISTRY[message_type]()
-        text = builder.build_message(data)
-        self._message = text
-        print(f'builde message setted: {text}')
-    
-    
-    @property
-    def message(self):
-        return self._message
-    
-    
-    def send(self):
-        print('\n', '-'*80)
+
+    def render_payload(self, message: NotificationCanonicalMessage):
+        return message.text
+
+    def send(self, payload):
+
+        print("\n", "-" * 80)
+
+        print("\nSending ...")
         
-        print('\nSending ...')
-        
-        print('\nmessage:')
-        print(f'    {self.message}')
-        
-        print('\nmessage reciever identifier:')
-        print(f'    {self._to}')
-        
-        print('\n', '-'*80, '\n')
+        print("\nmessage:")
+        print(f"    {payload}")
+
+        print("\nmessage reciever identifier:")
+        print(f"    {self._to}")
+
+        print("\n", "-" * 80, "\n")
