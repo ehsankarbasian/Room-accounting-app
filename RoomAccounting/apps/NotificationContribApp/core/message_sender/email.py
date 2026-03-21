555b94c5826db569a85c7f8d948b248dae1de686
Ehsan Karbasian <ehsan.karbasian@gmail.com>
2026-03-21 05:29:20 +0330

    Refactor email sender

--- RoomAccounting/apps/NotificationContribApp/core/message_sender/email.py
+++ RoomAccounting/apps/NotificationContribApp/core/message_sender/email.py
@@ -1,53 +1,45 @@
-from django.core.mail import send_mail as _send_mail
 from django.core.mail import EmailMultiAlternatives as _Email
 
 from core.settings import DEFAULT_FROM_EMAIL
 
-from apps.NotificationApp.core.registry import SenderRegistry, BuilderRegistry
-from apps.NotificationApp.core.interfaces import MessageSenderInterface, MessageBuilderInterface
+from apps.NotificationApp.core.registry import SenderRegistry
+from apps.NotificationApp.core.interfaces import MessageSenderInterface
+from apps.NotificationContribApp.core.models.canonical import NotificationCanonicalMessage
 
 from apps.NotificationContribApp.notification_types import SenderType
 
 
-def _send_html_email(subject, message, to_list, html_content):
-    email_obj = _Email(
-        subject=subject,
-        body=message,
-        from_email=DEFAULT_FROM_EMAIL,
-        to=to_list)
-    email_obj.attach_alternative(html_content, "text/html")
-    email_obj.send()
-
-
-def _send_text_email(subject, message, to_list):
-    _send_mail(
-        subject=subject,
-        message=message,
-        from_email=DEFAULT_FROM_EMAIL,
-        recipient_list=to_list)
-
-
 @SenderRegistry.register(name=SenderType.EMAIL)
 class EmailSender(MessageSenderInterface):
-    
-    def __init__(self, message_type, data, identifier):
+
+    def __init__(self, identifier):
         self._to = identifier
-        self.build_payload(message_type, data)
-    
-    def build_payload(self, message_type, data):
-        builder: MessageBuilderInterface = BuilderRegistry.REGISTRY[message_type]()
-        text = builder.build_message(data)
-        self._message = text
-        print(f'builde message setted: {text}')
-    
-    @property
-    def message(self):
-        return self._message
-    
-    def send(self):
-        print(f'Sending ... {self.message}')
-        html_content = self.message
-        _send_html_email(subject='reset password',
-                message='message',
-                to_list=[self._to],
-                html_content=html_content)
+
+    def render_payload(self, message: NotificationCanonicalMessage):
+
+        payload = {
+            "subject": "reset password",
+            "html_content": message.text,
+            "text_content": "message"
+        }
+
+        return payload
+
+
+    def send(self, payload):
+        
+        print(payload)
+
+        email_obj = _Email(
+            subject=payload["subject"],
+            body=payload["text_content"],
+            from_email=DEFAULT_FROM_EMAIL,
+            to=[self._to],
+        )
+
+        email_obj.attach_alternative(
+            payload["html_content"],
+            "text/html"
+        )
+
+        email_obj.send()
