91acfd9574090dcbb123c3f378a3e5555cb5bce6
Ehsan Karbasian <ehsan.karbasian@gmail.com>
2026-04-14 06:10:44 +0330

    Save hashed OTP tokens using NotificationToken model during send_otp

--- RoomAccounting/apps/NotificationApp/contrib/features/internal_features/otp/service.py
+++ RoomAccounting/apps/NotificationApp/contrib/features/internal_features/otp/service.py
@@ -1,4 +1,9 @@
+import hashlib
+from datetime import timedelta
+from django.utils import timezone
+
 from .....dispatching.dispatcher import NotificationDispatcher
+from .....models import NotificationToken
 
 from .message import OtpMessage
 
@@ -7,6 +12,15 @@ from .message import OtpMessage
 def send_otp(user, code: str):
     
     data = OtpMessage.Data(code=code)
+    
+    token_hash = hashlib.sha256(code.encode("utf-8")).hexdigest()
+
+    NotificationToken.objects.create(
+        selector=str(user.id),
+        token_hash=token_hash,
+        purpose="otp",
+        expires_at=timezone.now() + timedelta(minutes=2),
+    )
 
     NotificationDispatcher.send(
         user,
