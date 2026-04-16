12df872b44d23093d650fc653d53a843b35a7ee6
Ehsan Karbasian <ehsan.karbasian@gmail.com>
2026-04-16 23:22:28 +0330

    Debug: Support to set url_name attribute for ClassBasedViews

--- RoomAccounting/apps/NotificationApp/contrib/features/internal_routing/base.py
+++ RoomAccounting/apps/NotificationApp/contrib/features/internal_routing/base.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from types import FunctionType
+
 
 def build_feature_path(*, route, view, name):
     if not name:
@@ -7,5 +9,8 @@ def build_feature_path(*, route, view, name):
 
     # Bind url name into view for internal usage if needed
     view.url_name = name
+    
+    if not isinstance(view, FunctionType):
+        view = view.as_view()
 
     return path(route, view, name=name)
