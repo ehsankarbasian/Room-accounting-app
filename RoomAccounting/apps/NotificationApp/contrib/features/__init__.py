a318dcd9825b8184e943bb3fda2694fffbd17bde
Ehsan Karbasian <ehsan.karbasian@gmail.com>
2026-04-21 04:21:42 +0330

    Expose internal feature message classes via public API

--- RoomAccounting/apps/NotificationApp/contrib/features/__init__.py
+++ RoomAccounting/apps/NotificationApp/contrib/features/__init__.py
@@ -1,2 +1,12 @@
 from .internal.features.otp.service import send_otp, verify_otp
 from .internal.features.forgot_password.service import send_forgot_password
+
+from .internal.features.forgot_password.message import ForgotPasswordMessage as _ForgotPasswordMessage
+from .internal.features.verification.message import VerifyChannelMessage as _VerifyChannelMessage
+from .internal.features.otp.message import OtpMessage as _OtpMessage
+
+
+class MessageClasses:
+    ForgotPasswordMessage = _ForgotPasswordMessage
+    VerifyChannelMessage = _VerifyChannelMessage
+    OtpMessage = _OtpMessage
