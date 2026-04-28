46de0654b4ce77f549d762ff37efc923c89e9c12
Ehsan Karbasian <ehsan.karbasian@gmail.com>
2026-04-28 06:10:35 +0330

    Refactor: Delete Token model

--- RoomAccounting/apps/ReportApp/models/user.py
+++ RoomAccounting/apps/ReportApp/models/user.py
@@ -17,17 +17,3 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username + " (room_count:" + str(self.room_set.count()) + ")"
-
-
-class Token(models.Model):
-    verify_email_token = models.CharField(max_length=64, null=True)
-    verify_email_code = models.IntegerField(null=True)
-
-    reset_pass_token = models.CharField(max_length=64, null=True)
-    reset_pass_code = models.IntegerField(null=True)
-
-    class Meta:
-        verbose_name_plural = verbose_name_plural('Tokens')
-
-    def __str__(self):
-        return "Tokens of: " + self.user.username
