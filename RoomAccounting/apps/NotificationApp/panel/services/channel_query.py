5dc392a5940885e50ab7f405b8ab21738c2e7a78
Ehsan Karbasian <ehsan.karbasian@gmail.com>
2026-04-30 02:35:38 +0330

    Implement channel query services

--- RoomAccounting/apps/NotificationApp/panel/services/channel_query.py
+++ RoomAccounting/apps/NotificationApp/panel/services/channel_query.py
@@ -1,3 +1,30 @@
-# list_recipient_channels
-# get_channel
-# get_primary_channel
+from django.contrib.contenttypes.models import ContentType
+
+from NotificationApp.models import NotificationChannel
+from .exceptions import ChannelNotFound
+
+
+def list_recipient_channels(recipient):
+    recipient_content_type = ContentType.objects.get_for_model(recipient)
+
+    return NotificationChannel.objects.filter(
+        recipient_content_type=recipient_content_type,
+        recipient_object_id=recipient.pk,
+    ).order_by("priority")
+
+
+def get_channel(channel_id):
+    try:
+        return NotificationChannel.objects.get(id=channel_id)
+    except NotificationChannel.DoesNotExist:
+        raise ChannelNotFound()
+
+
+def get_primary_channel(recipient):
+    recipient_content_type = ContentType.objects.get_for_model(recipient)
+
+    return NotificationChannel.objects.filter(
+        recipient_content_type=recipient_content_type,
+        recipient_object_id=recipient.pk,
+        is_primary=True,
+    ).first()
