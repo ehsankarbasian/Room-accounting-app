0bb26e8a6e7acdf593eff88008f488a33d56d777
Ehsan Karbasian <ehsan.karbasian@gmail.com>
2026-04-29 08:00:23 +0330

    Make SmsSender class completed

--- RoomAccounting/apps/NotificationContribApp/senders/sms.py
+++ RoomAccounting/apps/NotificationContribApp/senders/sms.py
@@ -1,4 +1,8 @@
+from core.sms_client import send_text_sms
+
 from apps.NotificationApp.interfaces import MessageSenderInterface
+
+from ..message_schema import CanonicalMessage
 
 
 class SmsSender(MessageSenderInterface):
@@ -8,3 +12,30 @@ class SmsSender(MessageSenderInterface):
     """
     
     sender_key = "sms"
+    
+    @staticmethod
+    def render_payload(message: CanonicalMessage):
+        
+        buttons_as_text = ""
+        for button in message.buttons + message.button_links:
+            buttons_as_text += f'\n{button.text}:\n{button.target}\n'
+        
+        final_text = message.text + "\n" + buttons_as_text
+        return {"text": final_text}
+        
+    
+    @staticmethod
+    def send_payload(identifier: str, payload: dict):
+        
+        print("\n", "-" * 100)
+        print("\nSending ...\n")
+
+        print("SMS text: ", payload['text'])
+
+        print("\nmessage receiver identifier:")
+        print(f"    {identifier}")
+
+        print("\n", "-" * 100, "\n")
+        
+        
+        # send_text_sms(message=payload['text'], to=identifier)
