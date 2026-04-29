ec8553f9b561a0f40aeaabb25bd1d4bccd3648b8
Ehsan Karbasian <ehsan.karbasian@gmail.com>
2026-04-30 02:34:12 +0330

    Add service layer exceptions for channel operations

--- RoomAccounting/apps/NotificationApp/panel/services/exceptions.py
+++ RoomAccounting/apps/NotificationApp/panel/services/exceptions.py
@@ -1,6 +1,28 @@
-# ChannelAlreadyExists
-# UnsupportedChannelType
-# ChannelNotFound
-# ChannelAlreadyVerified
-# InvalidVerificationToken
-# VerificationTokenExpired
+
+class ChannelServiceException(Exception):
+    pass
+
+
+class ChannelAlreadyExists(ChannelServiceException):
+    pass
+
+
+class UnsupportedChannelType(ChannelServiceException):
+    pass
+
+
+class ChannelNotFound(ChannelServiceException):
+    pass
+
+
+class ChannelAlreadyVerified(ChannelServiceException):
+    pass
+
+
+class InvalidVerificationToken(ChannelServiceException):
+    pass
+
+
+class VerificationTokenExpired(ChannelServiceException):
+    pass
+
