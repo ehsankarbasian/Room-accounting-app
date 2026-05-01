3adeb363d83c60f12034d923e4a98602b9e5173c
Ehsan Karbasian <ehsan.karbasian@gmail.com>
2026-05-01 06:06:22 +0330

    Add (Manage Notification Channels) Button to HomePage

--- RoomAccounting/apps/NotificationApp/panel/urls.py
+++ RoomAccounting/apps/NotificationApp/panel/urls.py
@@ -3,7 +3,8 @@ from .views import channels
 
 
 urlpatterns = [
-    path("channels/", channels.channel_list_view, name="panel_channel_list"),
+    path("channels/", channels.channel_list_view, name="manage_channels"),
+    
     path("channels/create/", channels.create_channel_view, name="panel_channel_create"),
     path("channels/<int:channel_id>/delete/", channels.delete_channel_view, name="panel_channel_delete"),
     path("channels/<int:channel_id>/make-primary/", channels.make_primary_view, name="panel_channel_primary"),
